615785fad54960a45878f1b3919ac28e295356e5
Drew Dudash <drewdudash@gmail.com>
2013-03-08 22:04:06 -0600

    Refactored and optimized the box class with a template and local keywords.

--- box.lua
+++ box.lua
@@ -1,45 +1,56 @@
-box = {}
+require 'util'
 
-function box.new(x, y, width, height)
+Box = {}
+
+local boxTemplate = {}
+
+function boxTemplate:bottom()
+	return self.y + self.width
+end
+
+function boxTemplate:top()
+	return self.y
+end
+
+function boxTemplate:right()
+	return self.x + self.width
+end
+
+function boxTemplate:left()
+	return self.x
+end
+
+function boxTemplate:halfWidth()
+	return self.width / 2
+end
+
+function boxTemplate:halfHeight()
+	return self.height / 2
+end
+
+function boxTemplate:isColliding(other)
+	if self:left() < other:left() and other:left() < self:right() then
+		if self:top() < other:top() and other:top() < self:bottom() then
+			return true
+		elseif self:top() < other:bottom() and other:bottom() < self:bottom() then
+			return true
+		end
+	elseif self:left() < other:right() and other:right() < self:right() then
+		if self:top() < other:top() and other:top() < self:bottom() then
+			return true
+		elseif self:top() < other:bottom() and other:bottom() < self:bottom() then
+			return true
+		end
+	end
+	return false
+end
+
+function Box.new(x, y, width, height)
 	if not height then height = width end
-	self = {}
+	local self = table.shallow_copy(boxTemplate)
 	self.x = x
 	self.y = y
 	self.width = width
 	self.height = height
-	function self:bottom()
-		return self.y + self.width
-	end
-	function self:top()
-		return self.y
-	end
-	function self:right()
-		return self.x + self.width
-	end
-	function self:left()
-		return self.x
-	end
-	function self:halfWidth()
-		return self.width / 2
-	end
-	function self:halfHeight()
-		return self.height / 2
-	end
-	function self:isColliding(other)
-		if self:left() < other:left() and other:left() < self:right() then
-			if self:top() < other:top() and other:top() < self:bottom() then
-				return true
-			elseif self:top() < other:bottom() and other:bottom() < self:bottom() then
-				return true
-			end
-		elseif self:left() < other:right() and other:right() < self:right() then
-			if self:top() < other:top() and other:top() < self:bottom() then
-				return true
-			elseif self:top() < other:bottom() and other:bottom() < self:bottom() then
-				return true
-			end
-		end
-		return false
-	end
 	return self
 end
