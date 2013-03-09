01526e9e71a6a88b9c956616ef0967be1840e62c
Drew Dudash <drewdudash@gmail.com>
2013-03-08 23:32:24 -0600

    Added new resource files

--- resource.lua
+++ resource.lua
@@ -3,6 +3,8 @@ SOUND_DIR = 'resources/sound/'
 
 IMAGE = {
 		start_screen = love.graphics.newImage(IMAGE_DIR .. 'start_screen.png'),
+		start_button_up = love.graphics.newImage(IMAGE_DIR .. 'start_button_up.png'),
+		start_button_down = love.graphics.newImage(IMAGE_DIR .. 'star_button_down.png'),
 	}
 
 SOUND = {
